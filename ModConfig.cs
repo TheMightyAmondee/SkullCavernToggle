720b2ba02b0f212df55f8235248886865ec2560b
Amelie <amelie.parkinson.bates@gmail.com>
2021-02-01 16:45:44 +1100

    Add project files.

---
+++ ModConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI;
+
+namespace DifficultyToggle
+{
+    internal class ModConfig
+    {
+        public SButton ToggleDifficulty { get; set; } = SButton.Z;
+    }
+}
---
+++ ModEntry.cs
@@ -0,0 +1,68 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley.Network;
+using StardewValley;
+using System.Collections.Generic;
+
+namespace DifficultyToggle
+{
+    public class ModEntry
+        : Mod
+    {
+        readonly NetWorldState state = new NetWorldState();
+
+        private ModConfig config;
+
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += this.Toggle;
+            this.config = helper.ReadConfig<ModConfig>();
+        }
+
+        // Don't toggle if special order hasn't been completed or is active
+        private bool ShouldToggle()
+        {
+            var order = Game1.player.team.completedSpecialOrders;
+         
+            if(Game1.player.team.SpecialOrderActive("QiChallenge10") == true)
+            {
+                return false;
+            }
+
+            foreach (string soid in new List<string>(order.Keys))
+            {
+                if (soid.Contains("QiChallenge10") == true)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        // Toggle difficulty
+        private void Toggle(object sender, ButtonPressedEventArgs e)
+        {                       
+            if (e.Button == this.config.ToggleDifficulty && ShouldToggle() == true)
+            {
+                if(state.SkullCavesDifficulty == 1)
+                {
+                    state.SkullCavesDifficulty = 0;
+                    this.Monitor.Log($"Difficulty:{state.skullCavesDifficulty}");
+                }
+                else
+                {
+                    state.SkullCavesDifficulty = 1;
+                    this.Monitor.Log($"Difficulty:{state.skullCavesDifficulty}");
+                }
+
+                Game1.addHUDMessage(new HUDMessage("Skull Cavern Difficulty toggled", null));
+                Game1.netWorldState.Set(state);
+            }
+            else if(e.Button == SButton.Z && ShouldToggle() == false)
+            {
+                Game1.addHUDMessage(new HUDMessage("Skull Cavern Difficulty can't be toggled now", 3));
+            }
+        }
+    }
+}
